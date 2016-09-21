b9cf6135a10313bb6a51f33832434fa7aaf638b3
Colin Putney <colin@bettercompany.co>
2016-09-21 11:13:10 -0700

    Add support for grants

--- test/configuration.js
+++ test/configuration.js
@@ -1,8 +1,8 @@
 
-import Configuration from '../lib/configuration'
-import memfs from 'memfs'
 import path from 'path'
 
+import Configuration from '../lib/configuration'
+import {Migration, User} from '../lib/entities'
 import fix1 from './fixtures/one.js'
 
 const base = '/test'
@@ -10,19 +10,17 @@ const base = '/test'
 function vpath (relative) {
   return path.resolve(base, relative)
 }
-function mount (tree) {
-  Configuration.fs = new memfs.Volume()
-  Configuration.fs.mountSync(base, tree)
-  Configuration.fs.mkdirSync(`${base}/migrations`)
+function mount (fix) {
+  Configuration.fs = fix(base)
 }
 
 describe('configuration', () => {
-
   it('constructor', function () {
     mount(fix1)
     const conf = new Configuration(vpath('conf.json'))
     conf.directory.must.equal(base)
     conf.loc.must.have.property('migrations', `${base}/migrations`)
+    conf.loc.must.have.property('grants', `${base}/grants`)
   })
 
   it('migrations', function () {
@@ -33,12 +31,30 @@ describe('configuration', () => {
       migrations.length.must.equal(2)
 
       const first = migrations[0]
+      first.must.be.a(Migration)
       first.ordinal.must.equal(1)
-      first.name.must.equal('create-zilt')
+      first.name.must.equal('create-team')
 
       const second = migrations[1]
+      second.must.be.a(Migration)
       second.ordinal.must.equal(2)
-      second.name.must.equal('create-derp')
+      second.name.must.equal('create-game')
     })
   })
+
+  it('grants', function () {
+    mount(fix1)
+    const conf = new Configuration(vpath('conf.json'))
+    return conf.grants().then(grants => {
+      grants.must.be.an.array()
+      grants.length.must.equal(2)
+
+      const first = grants[0]
+      first.name.must.equal('reader')
+
+      const second = grants[1]
+      second.name.must.equal('writer')
+    })
+  })
+
 })
--- test/fixtures/one.js
+++ test/fixtures/one.js
@@ -1,16 +1,51 @@
+import memfs from 'memfs'
 
-export default tree = {
+export default function mount(base) {
+  const vol = new memfs.Volume()
+  vol.mountSync(base, tree)
+  vol.mkdirSync(`${base}/migrations`)
+  vol.mkdirSync(`${base}/grants`)
+  return vol
+}
+
+const tree = {
   'conf.json': JSON.stringify({
-    migrations: 'migrations'
+    migrations: 'migrations',
+    grants: 'grants'
   }),
-  'migrations/001-create-zilt.sql': 
-    'CREATE TABLE zilt (' +
-      'id serial PRIMARY KEY,' +
-      'value int DEFAULT 0 NOT NULL' +
-    ')', 
-  'migrations/002-create-derp.sql': 
-    'CREATE TABLE derp (' +
-      'id serial PRIMARY KEY,' +
-      'value text' +
-    ')'
+  'migrations/001-create-team.sql': `
+    CREATE TABLE team (
+      id serial PRIMARY KEY,
+      name text NOT NULL,
+      city text NOT NULL,
+    )`, 
+  'migrations/002-create-game.sql': `
+    CREATE TABLE game (
+      id serial PRIMARY KEY,
+      home_team integer NOT NULL REFERENCES team(id),
+      away_team integer NOT NULL REFERENCES team(id),
+      home_score smallint,
+      away_score smallint,
+      started timestamp with timezone,
+      CHECK positive_home_score (home_score >= 0),
+      CHECK positive_away_score (away_score >= 0)
+    )`,
+  'grants/reader.sql': `
+    ALTER ROLE $user SET search_path = $schema;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA $schema GRANT USAGE ON SEQUENCES TO $user;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA $schema GRANT SELECT TABLES TO $user;
+
+    GRANT USAGE ON SCHEMA $schema TO $user;
+    GRANT USAGE ON ALL SEQUENCES IN SCHEMA $schema TO $user;
+    GRANT SELECT ON ALL TABLES IN SCHEMA $schema TO $user;`,
+  'grants/writer.sql': `
+    ALTER ROLE $user SET search_path = $schema;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA $schema GRANT USAGE ON SEQUENCES TO $user;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA $schema 
+      GRANT SELECT, INSERT, UPDATE, DELETE TABLES TO $user;
+
+    GRANT USAGE ON SCHEMA $schema TO $user;
+    GRANT USAGE ON ALL SEQUENCES IN SCHEMA $schema TO $user;
+    GRANT SELECT, INSERT, UPDATE, DELETE 
+      ON ALL TABLES IN SCHEMA $schema TO $user;`
 }
