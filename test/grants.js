e90656b6cb73113b05249ebfbacf9db46b266efe
David Kaminsky <oursfs@gmail.com>
2019-03-14 13:29:41 -0700

    Updated to new linting standard (#6)
    * Updated to new linting standard
    
    * Fix build

--- test/grants.js
+++ test/grants.js
@@ -1,58 +1,67 @@
-import Configuration from '../lib/configuration'
-import vfs from './fixtures/simple.js'
+import Configuration from "../lib/configuration";
+import vfs from "./fixtures/simple.js";
 
-describe('grants', () => {
-  let c
-  let db
-  let conf
-  let hoops
+describe("grants", () => {
+  let c;
+  let db;
+  let conf;
+  let hoops;
 
   before(() => {
-    return Configuration.read('/test/conf.json', vfs)
+    return Configuration.read("/test/conf.json", vfs)
       .then(_conf => {
-        conf = _conf
-        db = conf.database('bft')
-        hoops = db.schema.hoops
-        return db.ensure()
-      }).then(() => {
-        return db.ensureUsers()
-      }).then(() => {
-        return db.connect()
-      }).then(client => {
-        c = client
-        return hoops.ensure(c)
+        conf = _conf;
+        db = conf.database("bft");
+        hoops = db.schema.hoops;
+        return db.ensure();
       })
-  })
+      .then(() => {
+        return db.ensureUsers();
+      })
+      .then(() => {
+        return db.connect();
+      })
+      .then(client => {
+        c = client;
+        return hoops.ensure(c);
+      });
+  });
 
   after(() => {
-    if (db) db.disconnect()
-  })
+    if (db) db.disconnect();
+  });
 
-  it('revokes privileges', function () {
-    return hoops.revokePrivileges(c)
+  it("revokes privileges", function() {
+    return hoops
+      .revokePrivileges(c)
       .then(() => hoops.getPrivileges(c))
       .then(grants => {
-        grants.length.must.equal(0)
-      })
-  })
+        grants.length.must.equal(0);
+      });
+  });
 
-  it('grants privileges', function () {
-    return hoops.grantPrivileges(c)
+  it("grants privileges", function() {
+    return hoops
+      .grantPrivileges(c)
       .then(() => hoops.getPrivileges(c))
       .then(grants => {
-        grants.length.must.equal(2)
+        grants.length.must.equal(2);
         grants.some(ea => {
-          return ea.usename === 'app1' &&
-            ea.kind === 'n' &&
+          return (
+            ea.usename === "app1" &&
+            ea.kind === "n" &&
             ea.name === hoops.name &&
-            ea.priv === 'USAGE'
-        })
+            ea.priv === "USAGE"
+          );
+        });
         grants.some(ea => {
-          return ea.usename === 'app2' &&
-            ea.kind === 'n' &&
+          return (
+            ea.usename === "app2" &&
+            ea.kind === "n" &&
             ea.name === hoops.name &&
-            ea.priv === 'USAGE'
-        })
-      })
-  })
-})
+            ea.priv === "USAGE"
+          );
+        });
+      });
+  });
+});
