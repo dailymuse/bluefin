1d340a85c921780b8531d232c5f2024cfbc1fa4d
David Kaminsky <oursfs@gmail.com>
2019-03-14 12:16:40 -0700

    Updated to new linting standard

--- test/cluster.js
+++ test/cluster.js
@@ -1,72 +1,71 @@
+import Client from "../lib/client";
+import Cluster from "../lib/cluster";
 
-import Client from '../lib/client'
-import Cluster from '../lib/cluster'
+describe("cluster", () => {
+  let conf;
+  let raw;
+  let cluster;
 
-describe('cluster', () => {
-  let conf
-  let raw
-  let cluster
-
-  function connectionTests () {
-    it('connects to default db', function () {
+  function connectionTests() {
+    it("connects to default db", function() {
       return cluster.connect().then(c => {
-        c.must.be.a(Client)
-        c.disconnect()
-      })
-    })
+        c.must.be.a(Client);
+        c.disconnect();
+      });
+    });
 
-    it('connects to postgres db', function () {
-      return cluster.connect('postgres').then(c => {
-        c.must.be.a(Client)
-        c.disconnect()
-      })
-    })
+    it("connects to postgres db", function() {
+      return cluster.connect("postgres").then(c => {
+        c.must.be.a(Client);
+        c.disconnect();
+      });
+    });
   }
 
   // can't test this in the docker environment we've created
-  describe.skip('no dsn', () => {
+  describe.skip("no dsn", () => {
     before(() => {
-      conf = {}
-      raw = {}
-      cluster = new Cluster(conf, 'nickname', raw)
-    })
+      conf = {};
+      raw = {};
+      cluster = new Cluster(conf, "nickname", raw);
+    });
 
-    connectionTests()
-  })
+    connectionTests();
+  });
 
   // can't test this in the docker environment we've created
-  describe.skip('localhost dsn', () => {
+  describe.skip("localhost dsn", () => {
     before(() => {
-      conf = {}
+      conf = {};
       raw = {
         dsn: {
-          host: 'localhost',
+          host: "localhost",
           port: 5432
         }
-      }
-      cluster = new Cluster(conf, 'nickname', raw)
-    })
-    connectionTests()
-  })
+      };
+      cluster = new Cluster(conf, "nickname", raw);
+    });
+    connectionTests();
+  });
 
-  describe('dsn with password', () => {
+  describe("dsn with password", () => {
     before(() => {
-      conf = { password: () => Promise.resolve('postgres') }
+      conf = { password: () => Promise.resolve("postgres") };
       raw = {
         dsn: {
-          host: 'pg',
-          user: 'postgres'
+          host: "pg",
+          user: "postgres"
         }
-      }
-      cluster = new Cluster(conf, 'nickname', raw)
-    })
+      };
+      cluster = new Cluster(conf, "nickname", raw);
+    });
 
-    it('connects to default db', function () {
+    it("connects to default db", function() {
       return cluster.connect().then(c => {
-        c.must.be.a(Client)
-        c.dsn.password.must.equal('postgres')
-        c.disconnect()
-      })
-    })
-  })
-})
+        c.must.be.a(Client);
+        c.dsn.password.must.equal("postgres");
+        c.disconnect();
+      });
+    });
+  });
+});
